14d64e1b5b128a0ed6134fb3c5c3e8e476844d8f
Michelle~ <theriomich@gmail.com>
2024-04-04 13:14:38 -0300

    Agregue nuevos cambios nav e iconos

--- router.js
+++ router.js
@@ -4,14 +4,15 @@ const router = express.Router()
 const conexion = require("./database/db")
 
 router.get("/", (req, res)=>{
-  conexion.query("SELECT * FROM users", (error, result)=>{
-    if(error){
-      throw error
-    }else{
-      res.send(result)
-    }
-  })
-})
+  res.render("index")
+//   conexion.query("SELECT * FROM users", (error, result)=>{
+//     if(error){
+//       throw error
+//     }else{
+//       res.send(result)
+//     }
+//   })
+ })
 
 
 module.exports= router;
