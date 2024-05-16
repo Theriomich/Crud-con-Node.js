78e6463c79f2a6f22954fd7c610916b9273d9c11
Michelle~ <theriomich@gmail.com>
2024-05-16 11:53:42 -0400

    Listo el edit

--- controllers/crud.js
+++ controllers/crud.js
@@ -12,3 +12,16 @@ conexion.query("INSERT INTO users SET ?", {user:user, rol:rol}, (error,result)=>
 })
 
 }
+//ACTUALIZAR un REGISTRO
+exports.update = (req, res)=>{
+  const id = req.body.id;
+  const user = req.body.user;
+  const rol = req.body.rol;
+  conexion.query('UPDATE users SET ? WHERE id = ?',[{user:user, rol:rol}, id], (error, results)=>{
+      if(error){
+          console.log(error);
+      }else{           
+          res.redirect('/');         
+      }
+});
+};
--- router.js
+++ router.js
@@ -18,7 +18,20 @@ router.get("/create", (req,res)=> {
   res.render("create")
 })
 
+//RUTA PARA EDITAR REGISTROS
+router.get('/edit/:id', (req,res)=>{    
+  const id = req.params.id;
+  conexion.query('SELECT * FROM users WHERE id=?',[id] , (error, results) => {
+      if (error) {
+          throw error;
+      }else{            
+          res.render('edit.ejs', {user:results[0]});            
+      }        
+  });
+});
+
 const crud = require("./controllers/crud")
 router.post("/save", crud.save)
+router.post("/update", crud.update)
 
 module.exports = router;
